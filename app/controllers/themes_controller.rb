db1f0c218c7de3cbb92a2c549bd95d7b2c082194
MidnightP <heijden.pvd@gmail.com>
2016-10-24 16:25:46 +0200

    themes_controller

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,26 +1,39 @@
 class ThemesController < ApplicationController
 
+  # homepage
   def index
-
+    @themes = Theme.all
   end
 
   def show
-
-  end
-
-  def delete
-
+    @theme = Theme.find(theme_params[:name])
   end
 
   def destroy
+    @theme = Theme.find(theme_params[:name])
 
+    @theme.destroy
+
+    redirect_to root_path
   end
 
   def edit
-
+    @theme = Theme.find(theme_params[:name])
   end
 
   def update
+    @theme = Theme.find(theme_params[:name])
 
+    if @theme.update_attributes( theme_params )
+      redirect_to theme_path(@theme), notice: "Update succesvol"
+    else
+      render :edit
+    end
   end
+
+  private
+  def theme_params
+    params.require(:theme).permit(:name, :style, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :vacations
+
+
+  get 'themes/new' => 'themes#new', as: :new_theme
+  get 'themes/:name/edit' => 'themes#edit', as: :edit_theme
+  get 'themes/:name' => 'themes#show', as: :theme
+  post 'themes' => 'themes#create'
+  patch 'themes/:name' => 'themes#update'
+  delete 'themes/:name' => 'themes#destroy'
+
+  root 'themes#index'
+
+
 end
