90621e620ec74def0d7d2ff6c733357ee9b37512
EsterKais <kaisester91@gmail.com>
2016-10-24 15:34:57 +0200

    Views

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,2 +1,26 @@
 class ThemesController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def delete
+
+  end
+
+  def destroy
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
