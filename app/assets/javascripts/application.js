399a510903655756686fe856f119b4fef37285fe
Sam Taal <Sam@Sams-Macbook.local>
2016-10-25 08:35:56 +0200

    Added dropdown and gradient

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider();
+  $('nav').click(function(){
+    $('.header--bottom').toggle();
+  })
+});
