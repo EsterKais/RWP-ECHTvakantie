fb3cdf46b2fb587f347ab246d2ff22434e67d0fa
Wouter de Vos <wouter@codaisseur.com>
2016-10-26 11:56:39 +0200

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # resources :vacations
-  # resources :themes
 
   resources :vacations, param: :title
+
   resources :themes, param: :name
+  get '/thema/:name/*filters' => "themes#show"
+
   resources :tphotos
   resources :vphotos
 
