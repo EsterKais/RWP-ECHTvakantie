c82258ea0e9e8b2ab62e4d93e52435c71e860e7a
MidnightP <heijden.pvd@gmail.com>
2016-10-27 20:53:28 +0200

    filter fun

--- app/helpers/regions_filter_helper.rb
+++ app/helpers/regions_filter_helper.rb
@@ -2,10 +2,15 @@ module RegionsFilterHelper
 
   def regions_filter
     @regions = Vacation.distinct(:region).pluck(:region)
+
+    html = []
+
     if @regions != []
       @regions.each do |region|
-        "<p>" +  link_to( "#{region}", "#{params[:name]}/land/#{region}" ) + "</p>"
+        html << ("<p>" +  link_to( "#{region}", "#{params[:name]}/land/#{region}" ) + "</p>")
       end
     end
+
+    return html
   end
 end
