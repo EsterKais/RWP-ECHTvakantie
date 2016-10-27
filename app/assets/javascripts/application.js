83ccca25418faacc3a95f3f1d568fc8180fc2f8f
MidnightP <heijden.pvd@gmail.com>
2016-10-27 22:21:58 +0200

    turbolinks fix, .any? for photos fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//
+//  TURBOLINKS BULLDOZERING THROUGH !!!
+//
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
