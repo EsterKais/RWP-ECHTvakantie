a93b9482c342ad50794e0f3706efb79e55d5ef32
MidnightP <heijden.pvd@gmail.com>
2016-10-27 21:58:31 +0200

    some testing

--- spec/features/admin_vacation_form_spec.rb
+++ spec/features/admin_vacation_form_spec.rb
@@ -33,23 +33,33 @@ feature 'Add vacation', js: true do
 
     # fill in the form CHECK FOR THE RIGHT FORM NAMES
     fill_in 'vacation_title', with: "Awesome Mountains and Valley's"
-    fill_in 'vacation_description', with: Faker::Lorem.sentence(40)
-    fill_in 'vacation_price', with: "€€€"
+
 
     fill_in 'vacation_country', with: "Nicaragua"
     fill_in 'vacation_region', with: "Ometepe"
 
-    fill_in 'vacation_review', with: Faker::Lorem.sentence(40)
+    fill_in 'vacation_address', with: Faker::Address.street_address
+
+    fill_in 'vacation_price', with: "Prijzig"
+
+    within '#vacation_price' do
+      find("option[value='Prijzig']").click
+    end
+
     fill_in 'vacation_show', with: true
 
     within '#vacation_themes_ids' do
       find("option[value='#{theme.id}']").click
     end
 
-    attach_file('photos_', File.join(Rails.root, '/spec/support/frank-nikes-2.jpg'))
+    fill_in 'vacation_description', with: Faker::Lorem.sentence(40)
+
+    fill_in 'vacation_review', with: Faker::Lorem.sentence(40)
+
+    attach_file('vphotos_', File.join(Rails.root, '/spec/support/frank-nikes-2.jpg'))
 
     # click save
-    click_button('Save')
+    click_button('Create Vacation')
 
     sleep(10)
 
