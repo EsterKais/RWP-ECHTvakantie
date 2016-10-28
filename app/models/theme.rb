eb38c81138bce05621fd8ea7fc0efd520e62dfaa
Sam Taal <Sam@Sams-Macbook.local>
2016-10-28 13:35:27 +0200

    fixed theme delete

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,5 +1,5 @@
 class Theme < ApplicationRecord
-  has_many :tphotos
+  has_many :tphotos, dependent: :destroy
   has_and_belongs_to_many :vacations
 
   accepts_nested_attributes_for :tphotos, reject_if: :all_blank?
