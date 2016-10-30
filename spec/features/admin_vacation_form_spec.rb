beefd8a3316904aa975ab617e214ff40ee711002
MidnightP <heijden.pvd@gmail.com>
2016-10-30 11:45:36 +0100

    tests almost working

--- spec/features/admin_vacation_form_spec.rb
+++ spec/features/admin_vacation_form_spec.rb
@@ -1,66 +1,69 @@
-# require 'rails_helper'
-# require 'capybara'
-#
-# feature 'Add vacation', js: true do
-#
-#   # clean DB first
-#   before do
-#     DatabaseCleaner.clean
-#   end
-#
-#   # user needs to be logged in to add a vacation
-#   before { login_as user }
-#   let ( :user) { create :user, email: "admin@test.com", password: "secret" }
-#
-#   ################ this should be another to acheive the same ###########
-#   ##                                                                   ##
-#   ##   user needs to be logged in to add a vacation                    ##
-#   ##   user = FactoryGirl.create(:user)                                ##
-#   ##   login_as(user, :scope => :user)                                 ##
-#   ##                                                                   ##
-#   #######################################################################
-#
-#   # create a tempt category
-#   let! ( :theme ) { create :theme, name: "Ordesa" }
-#
-#   scenario 'by filling in the form' do
-#     visit new_vacation_path
-#
-#     # fill in the form CHECK FOR THE RIGHT FORM NAMES
-#     fill_in 'vacation_title', with: "Awesome Mountains and Valley's"
-#
-#
-#     fill_in 'vacation_country', with: "Nicaragua"
-#     fill_in 'vacation_region', with: "Ometepe"
-#
-#     fill_in 'vacation_address', with: Faker::Address.street_address
-#
-#     # true!
-#     within '#vacation_show' do
-#       find("option[value='true']").click
-#     end
-#
-#     within '#vacation_price' do
-#       find("option[value='3']").click
-#     end
-#
-#     within '#vacation_theme_ids' do
-#       find("option[value='1']").click
-#     end
-#
-#     fill_in 'vacation_description', with: Faker::Lorem.sentence(40)
-#
-#     fill_in 'vacation_review', with: Faker::Lorem.sentence(40)
-#
-#     # attach_file('vphotos_', File.join(Rails.root, '/spec/support/frank-nikes-2.jpg'))
-#     # sleep(10)
-#
-#     # click save
-#     click_button('Create Vacation')
-#
-#     # expect to have a product in the db now
-#     # expect(Vacation.all.length).to eq(1)
-#     # expect(Vacation.first.title).to eq("Awesome Mountains and Valley's")
-#
-#   end
-# end
+require 'rails_helper'
+require 'capybara'
+
+feature 'Add vacation', js: true do
+
+  # clean DB first
+  before do
+    DatabaseCleaner.clean
+  end
+
+  # user needs to be logged in to add a vacation
+  before { login_as user }
+  let ( :user) { create :user, email: "admin@test.com", password: "secret" }
+
+  ################ this should be another to acheive the same ###########
+  ##                                                                   ##
+  ##   user needs to be logged in to add a vacation                    ##
+  ##   user = FactoryGirl.create(:user)                                ##
+  ##   login_as(user, :scope => :user)                                 ##
+  ##                                                                   ##
+  #######################################################################
+
+  # create a tempt category
+  let! ( :theme ) { create :theme, name: "Ordesa" }
+
+  scenario 'by filling in the form' do
+    visit new_vacation_path
+
+    # fill in the form CHECK FOR THE RIGHT FORM NAMES
+    fill_in 'vacation_title', with: "Awesome Mountains and Valley's"
+
+
+    fill_in 'vacation_country', with: "Nicaragua"
+    fill_in 'vacation_region', with: "Ometepe"
+
+    fill_in 'vacation_address', with: Faker::Address.street_address
+
+    # true!
+    within 'select#vacation_show' do
+      find("option[value='true']").click
+    end
+
+    within 'select#vacation_price' do
+      find("option[value='3']").click
+    end
+
+    within 'select#vacation_theme_ids' do
+      find("option[value='1']").click
+    end
+
+    fill_in 'vacation_description', with: Faker::Lorem.sentence(40)
+
+    fill_in 'vacation_review', with: Faker::Lorem.sentence(40)
+
+    # element is called vphotos
+    attach_file('vphotos_', File.join(Rails.root, '/spec/support/frank-nikes-2.jpg'))
+
+    # make sure the photo is uploaded to cloudinary
+    sleep(5)
+
+    # click save
+    click_button('Create Vacation')
+
+    # expect to have a product in the db now
+    expect(Vacation.all.length).to eq(1)
+    expect(Vacation.first.title).to eq("Awesome Mountains and Valley's")
+
+  end
+end
